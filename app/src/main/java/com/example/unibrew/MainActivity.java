541b20069d890fa17c940451cf08086dfb5461b7
Shamsuri Shazwan <shamsurishazwan@gmail.com>
2026-02-03 06:53:18 +0800

    Updated version

--- app/src/main/java/com/example/unibrew/MainActivity.java
+++ app/src/main/java/com/example/unibrew/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.unibrew;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -21,9 +22,16 @@ public class MainActivity extends AppCompatActivity {
             Fragment selected = null;
             if (item.getItemId() == R.id.nav_home) {
                 selected = new HomeFragment();
-            } else if (item.getItemId() == R.id.nav_profile) {
+            } else if (item.getItemId() == R.id.navigation_map) {
+                // Since you have 'activity_map_view.xml', I assume your class is named MapViewActivity
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+                return false;
+            }else if (item.getItemId() == R.id.nav_profile) {
                 selected = new ProfileFragment(); // We create this next!
+
             }
+
 
             if (selected != null) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selected).commit();
